2f3582f828be3b9fda295894edf852b97887d694
Zohebkhan804 <zoheb.dualsysco@gmail.com>
2025-07-23 13:11:28 +0530

    change achor tag to Link tags

--- src/app/Layout/Header.tsx
+++ src/app/Layout/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from 'next/link';
 import Home from '../Pages/Home';
 
 const Header = () => {
@@ -9,20 +10,20 @@ const Header = () => {
                 <div className="profile-img">
                     <img src="assets/img/Myimgs/zohebkhan1.jpg" alt='' className="img-fluid rounded-circle" />
                 </div>
-                <a href="/" className="logo d-flex align-items-center justify-content-center">
+                <Link href="/" className="logo d-flex align-items-center justify-content-center">
                     <h1 className="sitename">Zoheb khan</h1>
-                </a>
+                </Link>
                 <div className="social-links text-end">
-                    <a href="https://www.instagram.com/zubbu.khan_804/" className="instagram"><i className="bi bi-instagram" /></a>
+                    <Link href="https://www.instagram.com/zubbu.khan_804/" className="instagram"><i className="bi bi-instagram" /></Link>
                 </div>
                 <nav id="navmenu" className="navmenu">
                     <ul>
-                        <li><a href="#hero" className="active"><i className="bi bi-house navicon" />Home</a></li>
-                        <li><a href="/About"><i className="bi bi-person navicon" /> About</a></li>
-                        <li><a href="#resume"><i className="bi bi-file-earmark-text navicon" /> Resume</a></li>
-                        <li><a href="#portfolio"><i className="bi bi-images navicon" /> Portfolio</a></li>
-                        <li><a href="#services"><i className="bi bi-hdd-stack navicon" /> Services</a></li>
-                        <li><a href="#contact"><i className="bi bi-envelope navicon" /> Contact</a></li>
+                        <li><Link href="#hero" className="active"><i className="bi bi-house navicon" />Home</Link></li>
+                        <li><Link href="/About"><i className="bi bi-person navicon" /> About</Link></li>
+                        <li><Link href="#resume"><i className="bi bi-file-earmark-text navicon" /> Resume</Link></li>
+                        <li><Link href="#portfolio"><i className="bi bi-images navicon" /> Portfolio</Link></li>
+                        <li><Link href="#services"><i className="bi bi-hdd-stack navicon" /> Services</Link></li>
+                        <li><Link href="#contact"><i className="bi bi-envelope navicon" /> Contact</Link></li>
                     </ul>
                 </nav>
             </header>
